5280022995adf27abdc584ce09ccd8835b0ccfaf
ARoman <qpbd@ukr.net>
2016-12-13 18:43:51 +0200

    init

---
+++ qsAuthService.js
@@ -0,0 +1,90 @@
+var crypto = require('crypto');
+
+var service = {};
+
+//set keys after requirea
+//TODO: add error if not pass
+service.accessID = null;
+service.accessKey = null;
+
+service.genAuthorization = function(signature){
+  if(!signature){ throw "Missed signature"  }
+  //TODO: check keys
+  var authorization = "QS-HMAC-SHA256 " + service.accessID + ":" + signature;
+  return authorization;
+};
+
+service.genSignature = function(method, authpath, headers, params){
+  if(!method || !authpath || !headers|| !params){ throw "Missed params"  }
+  //TODO: check params and keys
+  // Append method
+  var stringToSign = "";
+  stringToSign += method.toUpperCase();
+
+  // Append Content-Type and Content-Md5
+  var ContentType = headers['Content-Type'] || "";
+  var ContentMD5 = headers['Content-MD5'] || "";
+  stringToSign += "\n" + ContentType + "\n" + ContentMD5;
+
+  // Append time
+  var dateStr = headers['X-QS-Date'] || "";
+  if (!dateStr) {
+    dateStr = params['X-QS-Date'] || "";
+    if (dateStr) {
+      stringToSign += "\n";
+    }
+  }
+  if (!dateStr) {
+    dateStr = headers['Date'] || "";
+    if (!dateStr) {
+      throw "Error: Date not set";
+    } else {
+      stringToSign += "\n" + dateStr;
+    }
+  }
+
+  // Generate canonicalized headers
+  var signedHeaders = genSignedHeaders(headers);
+  for (var item in signedHeaders.sort()) {
+    var key = signedHeaders[item];
+    stringToSign += "\n" + key.toLowerCase() +":" + headers[key];
+  }
+
+  // Generate canonicalized resource
+  var canonicalizedQuery = genCanonicalizedQuery(params);
+  var canonicalizedResource = authpath;
+  if (canonicalizedQuery) {
+    canonicalizedResource += "?" + canonicalizedQuery;
+  }
+  stringToSign += "\n" + canonicalizedResource;
+  var signature = crypto.createHmac('sha256', service.accessKey).update(
+    stringToSign).digest().toString('base64').trim();
+  return signature;
+
+};
+
+function genSignedHeaders(headers) {
+  var signedHeaders = [];
+  for (var key in headers) {
+    if (key.toLowerCase().indexOf('x-qs-') === 0) {
+      signedHeaders.push(headers[key]);
+    }
+  }
+  return signedHeaders;
+};
+
+function genCanonicalizedQuery(params) {
+  var canonicalizedQuery = "";
+  for (var key in params) {
+    if (canonicalizedQuery) {
+      canonicalizedQuery += "&";
+    }
+    canonicalizedQuery += encodeURIComponent(key);
+    if (params[key]) {
+      canonicalizedQuery += "=" + encodeURIComponent(params[key]);
+    }
+  }
+  return canonicalizedQuery;
+};
+
+module.exports = service;
---
+++ uploader.js
@@ -0,0 +1,58 @@
+var QsAuthService = require('./QsAuthService');
+var fs = require('fs');
+var http = require('http');
+
+//all what need to change on your side
+QsAuthService.accessID = "";
+QsAuthService.accessKey = "";
+var bucket = 'testbucket';
+
+//optional
+var path = 'new-image.png';
+var qingUrl = 'pek3a.qingstor.com';
+
+var headers = {};
+
+var fileStream = fs.createReadStream('test.png');
+var fileSize = fs.statSync('test.png').size;
+var fileContentType = 'image/' + 'png';
+
+var authPath = "/" + bucket + "/" + path;
+
+var headers = {};
+headers["Date"] = new Date().toUTCString();
+//when comment the next line its working and returns 201
+// headers["Content-Type"] = fileContentType;
+
+headers["Content-Length"] = fileSize;
+var signature = QsAuthService.genSignature("PUT", authPath, headers, {});
+//generating auth
+var authorization = QsAuthService.genAuthorization(signature);
+headers["Authorization"] = authorization;
+
+console.log("Headers", headers);
+console.log("Signature", signature);
+console.log("Authorization", authorization);
+
+var host = bucket + '.' + qingUrl;
+
+var options = {
+  hostname: host,
+  port: 80,
+  method: "PUT",
+  path: "/" + path,
+  headers: headers
+};
+
+var req = http.request(options, function(response) {
+
+  response.on('data', function(d) {
+    console.log("Status", response.statusCode, "Message", response.statusMessage);
+  });
+
+  response.on('end', function() {
+  	console.log("Status", response.statusCode, "Message", response.statusMessage);
+  });
+});
+
+fileStream.pipe(req);
